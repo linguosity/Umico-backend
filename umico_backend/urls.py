66af13968c840ce7e153b381689bc794ba20d463
linguosity <brandon@linguosity.ai>
2024-05-17 09:59:44 -0700

    tested successfully api endpoints for DJANGO Rest framework

--- umico_backend/urls.py
+++ umico_backend/urls.py
@@ -18,15 +18,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.models import User
-from rest_framework_nested import routers
-from rest_framework import routers, serializers, viewsets
-from .views import CustomerViewSet, ScanViewSet, PrintViewSet, FrameViewSet, AddressViewSet
+from rest_framework_nested.routers import NestedDefaultRouter
+from rest_framework import routers
+from umico_app.views import CustomerViewSet, ScanViewSet, PrintViewSet, FrameViewSet, AddressViewSet
 
 router = routers.DefaultRouter()
 router.register(r'customers', CustomerViewSet)
 
 #nested routers to add onto customer URL with PK id
-customers_router = routers.NestedDefaultRouter(router, r'customers', lookup='customer')
+customers_router = NestedDefaultRouter(router, r'customers', lookup='customer')
 customers_router.register(r'scans', ScanViewSet, basename='customer-scans')
 customers_router.register(r'frames', FrameViewSet, basename='customer-frames')
 customers_router.register(r'prints', PrintViewSet, basename='customer-prints')
@@ -36,7 +36,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     #
     path('api-auth/', include('rest_framework.urls')),
-
+    path('', include(router.urls)),
     path('', include(customers_router.urls)),
 ]
 
