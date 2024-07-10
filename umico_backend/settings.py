b2972841dbcbc9e77c7a2fc82cb6951fb685cac5
linguosity <brandon@linguosity.ai>
2024-07-09 20:04:50 -0700

    settings change

--- umico_backend/settings.py
+++ umico_backend/settings.py
@@ -110,7 +110,9 @@ MIDDLEWARE = [
 # CORS settings for Next.js frontend
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # IMPORTANT: Add your Next.js domain in production
+    "http://localhost:8000",
     "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
     "https://umicoframes-4f015aefac88.herokuapp.com",
     "https://umicoframesfe-eb05e3b38331.herokuapp.com"
 ]
